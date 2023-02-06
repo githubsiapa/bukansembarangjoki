687bb8c89eb1f1a5aa1f4231cf2b9c470c0db2f0
githubsiapa <eriyanto321@gmail.com>
2023-02-06 21:40:13 +0700

    first commit

---
+++ admin/pages/tables/penerbit/process/delete_penerbit.php
@@ -0,0 +1,22 @@
+<?php
+    // include DB connection file
+    include '../../../../../helper/connection.php';
+
+    // mendapatkan nilai dari form
+    $id_penerbit = $_GET['id_penerbit'];
+
+    $query = "DELETE FROM penerbit WHERE id_penerbit = '$id_penerbit'";
+
+    // menjalankan query isi data
+    if (mysqli_query($con, $query))
+    {
+        header("Location:../table_penerbit.php");
+    }
+    else
+    {
+        $error = urldecode("Data tidak berhasil dihapus");
+        header("Location:../table_penerbit.php?error=$error");
+    }
+
+    mysqli_close($con);
+?>
